ede2d77339b37876dc34bf2d374ba33dc147f72f
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2023-11-12 15:10:23 +0000

    Add CSS Reset

--- app/src/index.js
+++ app/src/index.js
@@ -1,13 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './app'
-import * as serviceWorker from './serviceWorker'
-import data from './data.json'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import "./reset.css";
+import App from "./app";
+import * as serviceWorker from "./serviceWorker";
+import data from "./data.json";
 
-ReactDOM.render(<App data={data} />, document.getElementById('root'))
+ReactDOM.render(<App data={data} />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister()
+serviceWorker.unregister();
