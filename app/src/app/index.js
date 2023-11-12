bd853c9d665ceb97c7386545c37d9733391cd979
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2023-11-12 16:48:30 +0000

    Add further content and styling - render owner img

--- app/src/app/index.js
+++ app/src/app/index.js
@@ -2,6 +2,8 @@ import React from 'react'
 import Image from './children/image'
 import LikeButton from './children/like_button'
 import styles from './styles'
+import UnlikedIcon from './children/like_button/children/unliked_icon'
+import LikedIcon from './children/like_button/children/liked_icon'
 
 const App = (props) => {
   const { data } = props
@@ -11,9 +13,37 @@ const App = (props) => {
         <Image data={data} />
       </div>
       <div style={styles.text}>
-        <p>User block</p>
-        <p>Comments block</p>
-        <LikeButton />
+        <div style={styles.profile}>
+          <img
+            style={styles.profileImg}
+            src={data.owner.profile_pic_url}
+            alt="AVFC Logo"
+          />
+          <div>
+            <h2>avfcofficial</h2>
+            <p>Villa Park, England</p>
+          </div>
+        </div>
+        <div>
+          <div style={styles.commentsList}>
+            <div style={styles.comments}>
+              <div style={styles.comments}>
+                <p style={styles.users}>User block</p>
+                <p>Comments block</p>
+              </div>
+              <button style={styles.button}>
+                <UnlikedIcon />
+              </button>
+            </div>
+            <div style={styles.comments}>
+              <div>
+                <p style={styles.users}>6,244 likes</p>
+                <p style={styles.date}>4 days ago</p>
+              </div>
+              <LikeButton />
+            </div>
+          </div>
+        </div>
       </div>
     </main>
   )
--- app/src/app/styles.js
+++ app/src/app/styles.js
@@ -27,5 +27,49 @@ export default {
     order: '2',
     flex: '1 0 40%',
     alignSelf: 'flex-start'
+  },
+
+  profile: {
+    display: 'flex',
+    alignItems: 'center',
+    gap: '1rem',
+    margin: '.625rem',
+    borderBottom: '.0625rem solid #eeeeef',
+    padding: '1.25rem 0'
+  },
+
+  profileImg: {
+    width: '3.125rem',
+    height: '3.125rem'
+  },
+
+  comments: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    gap: '.3125rem'
+  },
+
+  commentsList: {
+    margin: '0 .625rem'
+  },
+
+  users: {
+    fontWeight: 'bold'
+  },
+
+  button: {
+    display: 'inline-block',
+    backgroundColor: 'transparent',
+    border: 'none',
+    cursor: 'pointer',
+    padding: '0'
+  },
+
+  date: {
+    textTransform: 'uppercase',
+    color: '#a2a2a2',
+    fontSize: '.625rem',
+    marginTop: '.3125rem'
   }
 }
